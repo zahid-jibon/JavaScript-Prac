a728d19a625e9e5768847325600f3209c1675bd1
zahid <zahid10397@gmail.com>
2022-09-25 12:42:40 +0600

    array dec

---
+++ js/array.js
@@ -0,0 +1,16 @@
+var age = [10, 12, 13, 15, 20]
+
+console.log(age);
+
+var zahid = age[4];
+
+console.log("Zahid's age is:",zahid);
+
+age[2] = 16;
+
+console.log(age);
+
+var find = age.indexOf(25);
+
+console.log(find); 
+// This value can not be find as it is not asssign so the ans is -1
