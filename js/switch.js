7960e3d9b242b5af8dda159ea1e4d2f9a10d7c75
zahid <zahid10397@gmail.com>
2022-09-28 11:46:42 +0600

    switch js

---
+++ js/switch.js
@@ -0,0 +1,22 @@
+
+
+var num = 32;
+
+switch(num){
+    case 69:
+        console.log("This num is 69");
+        break;
+    case 70:
+        console.log("This num is 70");
+        break;
+    case 5:
+    case 10:
+        console.log("This num is either 5 or 10");
+        break;
+    case 36:
+        console.log("This num is 36");
+        break;
+    default:
+        console.log("This num is unknown");
+
+}
