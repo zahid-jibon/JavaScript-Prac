aad2fd4bc4797d33f7ddb38c9c1811c153689f87
zahid <zahid10397@gmail.com>
2022-09-28 11:06:28 +0600

    for loop

---
+++ js/for.js
@@ -0,0 +1,17 @@
+
+
+var i;
+
+for(i = 0; i <= 15; i++){
+    console.log(i);
+}
+
+
+var name = ['Zahid', 'Hossain', 'Jibon','Mahadi','Sajid']
+
+name.push('Kashem', 'Arnob');
+
+for(var i=0; i< name.length; i++){
+    var friends = name[i];
+    console.log(friends);
+}
